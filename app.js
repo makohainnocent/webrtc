6976fd116736e848817e85d3527c94ef716e8325
makohainnocent <vantagepoint2019@gmail.com>
2020-04-12 15:43:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-    var chatInterface = document.getElementById('chat-interface');
+    const chatInterface = document.getElementById('chat-interface');
     const myVideoSample = document.getElementById('my-video-sample');
     const myVideo = document.getElementById('my-video');
     const remoteVideo = document.getElementById('remote-video');
